a67497fb4b7d34293dc2e10b765dd4845d14d1be
João Matos <aluno21887@ipt.pt>
2022-03-23 15:27:25 +0000

    implementações de funcionalidades no controller

--- Calculator/Calculator/Controllers/HomeController.cs
+++ Calculator/Calculator/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Calculator.Models;
+﻿
+using Calculator.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -13,18 +14,131 @@ namespace Calculator.Controllers
             _logger = logger;
         }
 
-
-        [HttpGet] // este anotador é facultativo
-        // return View(), mas qual view?, o codigo procura a view com o nome "Index"
+        [HttpGet] //opcional -> se não se especificar, escolhe sempre o Get
         public IActionResult Index()
         {
+            //preparar os dados para serem enviados para a View na primeira interação
+            ViewBag.Visor = "0";
+
             return View();
         }
-        [HttpPost] // só quando o formulario for submetido em "post" ele sera acionado
-        public IActionResult Index(String botao)
+
+        [HttpPost] //Quando o formulário for submetido em 'post', ele será acionado
+        public IActionResult Index(string botao, string visor, string primeiroOperando, string operador, double result, string LimpaEcra)
         {
+
+
+            //testar valor do 'botao'
+
+            switch (botao)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                    //pressionou um algarismo
+                    //Sugestão -> Fazer de forma algébrica
+                    if (visor == "0" || LimpaEcra == "sim")
+                    {
+                        visor = botao;
+                    }
+                    else
+                    {
+                        LimpaEcra = "nao";
+                        visor += botao;
+                    }
+                    break;
+
+                case ",":
+                    if (!visor.Contains(','))
+                    {
+                        visor += botao;
+                    }
+                    break;
+
+                case "+/-":
+                    //vai inverter o valor do visor 
+                    //Sugestão -> Fazer de forma algébrica
+                    if (!visor.StartsWith("-")) // ou então !visor.Contains("-")
+                    {
+                        visor = "-" + visor;
+                    }
+                    else
+                    {
+                        visor = visor[1..]; //Semelhante a visor.SubString(1) 
+                                            //ou então visor.Remove(0, 1);-> Remove do indice 0 ao indice 1 (exclusive)
+                    }
+                    break;
+
+                case "C":
+                    visor = "0";
+                    primeiroOperando = "0";
+                    operador = "0";
+                    break;
+
+                case "+":
+                    if(operador != "0") {
+                        switch (operador)
+                        {
+                            case "+":
+
+                                result = Convert.ToDouble(visor) + Convert.ToDouble(primeiroOperando);
+                                
+
+                                break;
+                            case "-":
+                                result =Convert.ToDouble(visor) - Convert.ToDouble(primeiroOperando);
+                                
+                                break;
+
+                            case "*":
+                                result = Convert.ToDouble(visor) * Convert.ToDouble(primeiroOperando);
+                                
+                                break;
+                            case ":":
+                                result = Convert.ToDouble(visor) / Convert.ToDouble(primeiroOperando);
+                               
+                                break;
+                                visor = Convert.ToString(result);
+                                primeiroOperando = visor;
+
+
+                        }
+                    
+                    
+                    
+                    }
+
+                    primeiroOperando = visor;
+                    visor = "0";
+                    operador = botao;
+
+                    break;
+
+                case "-":
+                case "*":
+                case ":":
+                    primeiroOperando = visor;
+                    operador = botao;
+
+
+                    break;
+
+            }
+
+            //preparar os dados para serem enviados para a View
+            ViewBag.Visor = visor;
+            ViewBag.PrimeiroOperando = primeiroOperando;
+            ViewBag.Operador = operador;
             return View();
         }
+
         public IActionResult Privacy()
         {
             return View();
